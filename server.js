a49904649f6ddfddff69da889f87e7390670075d
kiyoshi-san <kiyoshibudokan@hotmail.com>
2022-04-28 01:10:47 -0300

    Solving CORS and Enviroment variables by dotenv

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import productRoute from './routes/product.js';
+import path from 'path';
 import dotenv from "dotenv";
+import cors from "cors";
 
-dotenv.config();
+//SET STATIC DIR
+const __dirname = path.resolve();
+dotenv.config({ path: __dirname + "/.env.local" });
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
